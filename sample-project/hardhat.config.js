109922f74c40da68c7b9ec914ce53055b833ef08
Luis Masuelli <luisfmasuelli@gmail.com>
2024-12-15 20:04:12 -0500

    Loading the plugins in the config file.

--- sample-project/hardhat.config.js
+++ sample-project/hardhat.config.js
@@ -1,4 +1,8 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("hardhat-enquirer-plus");
+require("hardhat-common-tools");
+require("hardhat-blueprints");
+require("..");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
