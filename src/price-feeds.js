1a5a3d058ea245a7ea0d2ace6b1b3e5ba59719c7
Luis Masuelli <luisfmasuelli@gmail.com>
2024-12-15 21:32:53 -0500

    Adding the price-feed-related methods.

--- src/price-feeds.js
+++ src/price-feeds.js
@@ -19,6 +19,59 @@ extendEnvironment((hre) => {
             solidityVersionArgument
         ]
     );
+
+    new hre.methodPrompts.ContractMethodPrompt(
+        "call", "latestRoundData", {
+            onError: (e) => {
+                console.error("There was an error while running this method");
+                console.error(e);
+            },
+            onSuccess: (value) => {
+                console.log("Answer:", value);
+            }
+        }, [], {}
+    ).asTask(
+        "chainlink:price-feed:latest-round-data",
+        "Invokes latestRoundData() on an AggregatorV3Interface-implementing (i.e. Price Feed) contract"
+    );
+    new hre.methodPrompts.ContractMethodPrompt(
+        "call", "getRoundData", {
+            onError: (e) => {
+                console.error("There was an error while running this method");
+                console.error(e);
+            },
+            onSuccess: (value) => {
+                console.log("Answer:", value);
+            }
+        }, [{
+            name: "roundId",
+            description: "The id of the round",
+            message: "What's the round id?",
+            argumentType: "uint80"
+        }], {}
+    ).asTask(
+        "chainlink:price-feed:get-round-data",
+        "Invokes getRoundData(uint80) on an AggregatorV3Interface-implementing (i.e. Price Feed) contract"
+    );
+    new hre.methodPrompts.ContractMethodPrompt(
+        "send", "setAnswer", {
+            onError: (e) => {
+                console.error("There was an error while running this method");
+                console.error(e);
+            },
+            onSuccess: (tx) => {
+                console.log("Answer set successfully. Transaction is:", tx);
+            }
+        }, [{
+            name: "answer",
+            description: "The value to set",
+            message: "What's the new value to set?",
+            argumentType: "int256"
+        }], {}
+    ).asTask(
+        "chainlink:price-feed:set-answer",
+        "Invokes setAnswer(int256) on an AggregatorV3Interface-implementing (i.e. Price Feed) contract"
+    );
 });
 
 module.exports = {};
