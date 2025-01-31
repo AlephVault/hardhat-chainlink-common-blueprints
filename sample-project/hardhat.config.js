012e062a9005a7cadb845df0d419c41cd3d2387b
Luis Masuelli <luisfmasuelli@gmail.com>
2025-01-30 21:43:55 -0500

    Adding sample networks for the sample project.

--- sample-project/hardhat.config.js
+++ sample-project/hardhat.config.js
@@ -9,4 +9,19 @@ require("..");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    hardhat: {
+      chainId: 31337 // Default Hardhat Network ID
+    },
+    amoy: {
+      url: "https://polygon-amoy-bor-rpc.publicnode.com", // Replace with your provider
+      accounts: [], // process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      chainId: 80002
+    },
+    polygon: {
+      url: "https://polygon-rpc.com", // Replace with your provider
+      accounts: [], // process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      chainId: 137 // Polygon Mainnet Chain ID
+    }
+  }
 };
