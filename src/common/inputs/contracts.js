bdfb34b710aac45a5368c51cd05b588cc2b9dfa0
Luis Masuelli <luisfmasuelli@gmail.com>
2025-01-30 19:06:14 -0500

    Adding some auto-imports for the common contracts.

---
+++ src/common/inputs/contracts.js
@@ -0,0 +1,33 @@
+import {extendEnvironment} from "hardhat/config";
+
+extendEnvironment(hre => {
+    /**
+     * Lets the user pick an existing contract address
+     * among the available contract addresses of a
+     * given type.
+     */
+    class GivenOrRemoteContractSelect extends hre.enquirerPlus.Enquirer.GivenOrSelect {
+        constructor({hre, loader, ...options}) {
+            super({...options, choices: ["Loading..."]});
+            this._loadChoices = async () => {
+                const chainId = await hre.common.getChainId();
+                return await loader().filter((e) => chainId === e.chainId).map((e) => {
+                    return {name: e.address, message: e.name};
+                });
+            }
+        }
+
+        async run() {
+            this.choices = await this._loadChoices();
+            this.options.choices = deployedContracts;
+            return await super.run();
+        }
+    }
+
+    hre.enquirerPlus.Enquirer.GivenOrRemoteContractSelect = GivenOrRemoteContractSelect;
+    hre.enquirerPlus.utils.registerPromptClass(
+        "plus:hardhat:given-or-remote-contract-select", GivenOrRemoteContractSelect
+    );
+})
+
+module.exports = {};
