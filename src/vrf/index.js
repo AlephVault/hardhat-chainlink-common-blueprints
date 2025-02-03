cf59d98feb25ced3e29a925d43e091bedc6d79b4
Luis Masuelli <luisfmasuelli@gmail.com>
2025-02-03 13:22:05 -0500

    Conditioning some tasks and blueprints.

--- src/vrf/index.js
+++ src/vrf/index.js
@@ -47,107 +47,6 @@ extendEnvironment((hre) => {
             }
         ]
     );
-    hre.blueprints.registerBlueprint(
-        "chainlink:vrf:coordinator-mock-contract", "VRFCoordinatorV2PlusMock", "A Chainlink VRFCoordinatorV2_5 mock contract",
-        path.resolve(baseDir, "solidity", "VRFCoordinatorV2_5Mock.sol.template"), "solidity", [
-            solidityVersionArgument,
-            {
-                name: "BASE_FEE",
-                description: "The base fee for requests, expressed in LINK fractions",
-                message: "Choose a base fee for the requests",
-                argumentType: "uint96",
-                initial: 100000000000000000n
-            },
-            {
-                name: "GAS_PRICE",
-                description: "The gas price for requests, expressed in LINK fractions",
-                message: "Choose a gas price for the requests",
-                argumentType: "uint96",
-                initial: 1000000000n
-            },
-            {
-                name: "WEI_PER_UNIT_LINK",
-                description: "The LINK/NATIVE rate",
-                message: "Choose the amount of native wei that costs a single LINK token",
-                argumentType: "int256",
-                initial: 7500000000000000n
-            }
-        ]
-    );
-    hre.blueprints.registerBlueprint(
-        "chainlink:vrf:consumer-deployment", "VRFConsumerV2Plus", "An ignition module for a new Chainlink VRFConsumerV2Plus contract to be deployed in the local network",
-        path.resolve(baseDir, "ignition-modules", "VRFConsumerV2Plus.js.template"), "ignition-module", [
-            {
-                name: "CONTRACT_NAME",
-                description: "The type to use for the contract",
-                message: "Choose one of your contract artifacts (must be a VRFConsumerV2Plus contract)",
-                argumentType: "contract"
-            },
-            {
-                name: "SUBSCRIPTION_ID",
-                description: "The id of the subscription to use",
-                message: "Choose the id of the subscription to use (it can be an arbitrary value)",
-                argumentType: "uint256"
-            },
-            {
-                name: "VRF_COORDINATOR",
-                description: "The VRFCoordinator contract",
-                message: "Choose the proper VRF Coordinator contract",
-                argumentType: "contract"
-            }
-        ]
-    );
-    hre.blueprints.registerBlueprint(
-        "chainlink:vrf:consumer-nonlocal-deployment", "VRFConsumerV2Plus", "An ignition module for a new Chainlink VRFConsumerV2Plus contract to be deployed in a remote network",
-        path.resolve(baseDir, "ignition-modules", "VRFConsumerV2Plus.NonLocal.js.template"), "ignition-module", [
-            {
-                name: "CONTRACT_NAME",
-                description: "The type to use for the contract",
-                message: "Choose one of your contract artifacts (must be a VRFConsumerV2Plus contract)",
-                argumentType: "contract"
-            },
-            {
-                name: "SUBSCRIPTION_IGNITION_PARAMETER",
-                initial: "subscriptionId",
-                description: "The name of the ignition parameter that will hold the subscription id for this contract",
-                message: "Choose a name for an ignition parameter that will hold the subscription id for this contract",
-                argumentType: "identifier"
-            },
-            {
-                name: "VRF_COORDINATOR",
-                description: "The VRFCoordinator contract",
-                message: "Choose the proper VRF Coordinator contract",
-                argumentType: {
-                    type: "plus:hardhat:given-or-remote-value-select",
-                    remoteValueType: "VRF Coordinator contracts",
-                    loader: () => getVRFCoordinators()
-                }
-            },
-            {
-                name: "KEY_HASH",
-                description: "The gas lane to use",
-                message: "Choose the proper gas lane to use (check Chainlink's docs for the network to understand the gas prices)",
-                argumentType: {
-                    type: "plus:hardhat:given-or-remote-value-select",
-                    remoteValueType: "VRF gas lanes",
-                    loader: () => getVRFLaneHashes()
-                }
-            }
-        ]
-    );
-    hre.blueprints.registerBlueprint(
-        "chainlink:vrf:coordinator-mock-deployment", "VRFCoordinatorV2_5Mock", "An ignition module for a new Chainlink VRFCoordinatoeV2_5Mock contract to be deployed in the local network",
-        path.resolve(baseDir, "ignition-modules", "VRFCoordinatorV2_5Mock.js.template"),
-        "ignition-module", [
-            {
-                name: "CONTRACT_NAME",
-                description: "The type to use for the contract",
-                message: "Choose one of your contract artifacts (it must be a VRFCoordinatorV2_5Mock contract)",
-                argumentType: "contract"
-            }
-        ]
-    );
-
     new hre.methodPrompts.ContractMethodPrompt(
         "send", "addConsumer", {
             onError: (e) => {
@@ -202,56 +101,160 @@ extendEnvironment((hre) => {
         "chainlink:vrf:remove-subscription-consumer",
         "Invokes removeConsumer on a VRF coordinator 2.5 contract for a given VRFConsumerV2Plus contract"
     );
-    new hre.methodPrompts.ContractMethodPrompt(
-        "send", "fundSubscription", {
-            onError: (e) => {
-                console.error("There was an error while running this method");
-                console.error(e);
-            },
-            onSuccess: (tx) => {
-                console.log("tx:", tx);
+
+    if (["hardhat", "localhost"].includes(hre.network.name)) {
+        hre.blueprints.registerBlueprint(
+            "chainlink:vrf:consumer-deployment", "VRFConsumerV2Plus", "An ignition module for a new Chainlink VRFConsumerV2Plus contract to be deployed in the local network",
+            path.resolve(baseDir, "ignition-modules", "VRFConsumerV2Plus.js.template"), "ignition-module", [
+                {
+                    name: "CONTRACT_NAME",
+                    description: "The type to use for the contract",
+                    message: "Choose one of your contract artifacts (must be a VRFConsumerV2Plus contract)",
+                    argumentType: "contract"
+                },
+                {
+                    name: "SUBSCRIPTION_ID",
+                    description: "The id of the subscription to use",
+                    message: "Choose the id of the subscription to use (it can be an arbitrary value)",
+                    argumentType: "uint256"
+                },
+                {
+                    name: "VRF_COORDINATOR",
+                    description: "The VRFCoordinator contract",
+                    message: "Choose the proper VRF Coordinator contract",
+                    argumentType: "contract"
+                }
+            ]
+        );
+        hre.blueprints.registerBlueprint(
+            "chainlink:vrf:coordinator-mock-contract", "VRFCoordinatorV2PlusMock", "A Chainlink VRFCoordinatorV2_5 mock contract",
+            path.resolve(baseDir, "solidity", "VRFCoordinatorV2_5Mock.sol.template"), "solidity", [
+                solidityVersionArgument,
+                {
+                    name: "BASE_FEE",
+                    description: "The base fee for requests, expressed in LINK fractions",
+                    message: "Choose a base fee for the requests",
+                    argumentType: "uint96",
+                    initial: 100000000000000000n
+                },
+                {
+                    name: "GAS_PRICE",
+                    description: "The gas price for requests, expressed in LINK fractions",
+                    message: "Choose a gas price for the requests",
+                    argumentType: "uint96",
+                    initial: 1000000000n
+                },
+                {
+                    name: "WEI_PER_UNIT_LINK",
+                    description: "The LINK/NATIVE rate",
+                    message: "Choose the amount of native wei that costs a single LINK token",
+                    argumentType: "int256",
+                    initial: 7500000000000000n
+                }
+            ]
+        );
+        hre.blueprints.registerBlueprint(
+            "chainlink:vrf:coordinator-mock-deployment", "VRFCoordinatorV2_5Mock", "An ignition module for a new Chainlink VRFCoordinatoeV2_5Mock contract to be deployed in the local network",
+            path.resolve(baseDir, "ignition-modules", "VRFCoordinatorV2_5Mock.js.template"),
+            "ignition-module", [
+                {
+                    name: "CONTRACT_NAME",
+                    description: "The type to use for the contract",
+                    message: "Choose one of your contract artifacts (it must be a VRFCoordinatorV2_5Mock contract)",
+                    argumentType: "contract"
+                }
+            ]
+        );
+    } else {
+        hre.blueprints.registerBlueprint(
+            "chainlink:vrf:consumer-deployment", "VRFConsumerV2Plus", "An ignition module for a new Chainlink VRFConsumerV2Plus contract to be deployed in a remote network",
+            path.resolve(baseDir, "ignition-modules", "VRFConsumerV2Plus.NonLocal.js.template"), "ignition-module", [
+                {
+                    name: "CONTRACT_NAME",
+                    description: "The type to use for the contract",
+                    message: "Choose one of your contract artifacts (must be a VRFConsumerV2Plus contract)",
+                    argumentType: "contract"
+                },
+                {
+                    name: "SUBSCRIPTION_IGNITION_PARAMETER",
+                    initial: "subscriptionId",
+                    description: "The name of the ignition parameter that will hold the subscription id for this contract",
+                    message: "Choose a name for an ignition parameter that will hold the subscription id for this contract",
+                    argumentType: "identifier"
+                },
+                {
+                    name: "VRF_COORDINATOR",
+                    description: "The VRFCoordinator contract",
+                    message: "Choose the proper VRF Coordinator contract",
+                    argumentType: {
+                        type: "plus:hardhat:given-or-remote-value-select",
+                        remoteValueType: "VRF Coordinator contracts",
+                        loader: () => getVRFCoordinators()
+                    }
+                },
+                {
+                    name: "KEY_HASH",
+                    description: "The gas lane to use",
+                    message: "Choose the proper gas lane to use (check Chainlink's docs for the network to understand the gas prices)",
+                    argumentType: {
+                        type: "plus:hardhat:given-or-remote-value-select",
+                        remoteValueType: "VRF gas lanes",
+                        loader: () => getVRFLaneHashes()
+                    }
+                }
+            ]
+        );
+        new hre.methodPrompts.ContractMethodPrompt(
+            "send", "fundSubscription", {
+                onError: (e) => {
+                    console.error("There was an error while running this method");
+                    console.error(e);
+                },
+                onSuccess: (tx) => {
+                    console.log("tx:", tx);
+                }
+            }, [
+                {
+                    name: "subscriptionId",
+                    description: "The id of the subscription to fund (with LINK)",
+                    message: "Choose the id of the subscription to fund (with LINK)",
+                    argumentType: "uint256"
+                },
+                {
+                    name: "amount",
+                    description: "The amount to FUND, expressed in LINK fractions",
+                    message: "Choose the amount to FUND (in fractions - 1e18 means 1 LINK)",
+                    argumentType: "uint256"
+                }
+            ], {}
+        ).asTask(
+            "chainlink:vrf:fund-subscription",
+            "Invokes fundSubscription on a mock VRF coordinator 2.5 contract, incrementing the LINK balance"
+        );
+        new hre.methodPrompts.ContractMethodPrompt(
+            "send", "fundSubscriptionWithNative", {
+                onError: (e) => {
+                    console.error("There was an error while running this method");
+                    console.error(e);
+                },
+                onSuccess: (tx) => {
+                    console.log("tx:", tx);
+                }
+            }, [
+                {
+                    name: "subscriptionId",
+                    description: "The id of the subscription to fund (with LINK)",
+                    message: "Choose the id of the subscription to fund (with LINK)",
+                    argumentType: "uint256"
+                }
+            ], {
+                value: {onAbsent: "prompt"}
             }
-        }, [
-            {
-                name: "subscriptionId",
-                description: "The id of the subscription to fund (with LINK)",
-                message: "Choose the id of the subscription to fund (with LINK)",
-                argumentType: "uint256"
-            },
-            {
-                name: "amount",
-                description: "The amount to FUND, expressed in LINK fractions",
-                message: "Choose the amount to FUND (in fractions - 1e18 means 1 LINK)",
-                argumentType: "uint256"
-            }
-        ], {}
-    ).asTask(
-        "chainlink:vrf:fund-subscription",
-        "Invokes fundSubscription on a mock VRF coordinator 2.5 contract, incrementing the LINK balance"
-    );
-    new hre.methodPrompts.ContractMethodPrompt(
-        "send", "fundSubscriptionWithNative", {
-            onError: (e) => {
-                console.error("There was an error while running this method");
-                console.error(e);
-            },
-            onSuccess: (tx) => {
-                console.log("tx:", tx);
-            }
-        }, [
-            {
-                name: "subscriptionId",
-                description: "The id of the subscription to fund (with LINK)",
-                message: "Choose the id of the subscription to fund (with LINK)",
-                argumentType: "uint256"
-            }
-        ], {
-            value: {onAbsent: "prompt"}
-        }
-    ).asTask(
-        "chainlink:vrf:fund-subscription-with-native",
-        "Invokes fundSubscription on a mock VRF coordinator 2.5 contract, incrementing the native balance"
-    );
+        ).asTask(
+            "chainlink:vrf:fund-subscription-with-native",
+            "Invokes fundSubscription on a mock VRF coordinator 2.5 contract, incrementing the native balance"
+        );
+    }
 
     //         args: deploymentId, deployedContractId
     //
