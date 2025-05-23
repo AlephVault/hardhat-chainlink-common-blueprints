fd21ebe89051ec5727eb3bd7e3d018b90d982585
Luis Masuelli <luisfmasuelli@gmail.com>
2025-05-23 17:40:43 -0500

    Removing some sample code.

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,11 +1,38 @@
 const path = require("path");
 const { extendEnvironment } = require("hardhat/config");
 const { getRouters, getDONIDs } = require("./utils/download");
+const { simulateScript, decodeResult } = require("@chainlink/functions-toolkit");
 
 const baseDir = path.resolve(
     __dirname, "..", "..", "data", "templates"
 );
 
 extendEnvironment((hre) => {
-
+    // task("simulate")
+    //     .setAction(async () => {
+    //         console.log(await simulateScript({
+    //             source: "return Functions.encodeString('Hello World');",
+    //             secrets: {},
+    //             args: [],
+    //             bytesArgs: []
+    //         }));
+    //     });
+    // new hre.methodPrompts.CustomPrompt(
+    //     async function() {
+    //         return await simulateScript({
+    //             source: "return Functions.encodeString('Hello World')",
+    //             secrets: {},
+    //             args: [],
+    //             bytesArgs: []
+    //         });
+    //     }, {
+    //         onError: (e) => {
+    //             console.error("There was an error while getting the balance");
+    //             console.error(e);
+    //         },
+    //         onSuccess: (value) => {
+    //             console.log("Result:", value);
+    //         }
+    //     }, [], {}
+    // ).asTask("simulate", "Simulates a JS/Deno function call", {onlyExplicitTxOptions: true});
 });
